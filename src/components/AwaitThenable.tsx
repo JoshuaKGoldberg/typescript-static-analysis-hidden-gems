6180c5d88988f195a965f8d31de9f20013c4de65
Josh Goldberg <git@joshuakgoldberg.com>
2022-11-02 15:09:27 -0400

    chore: reset AwaitThenable back

--- src/components/AwaitThenable.tsx
+++ src/components/AwaitThenable.tsx
@@ -1,8 +1,22 @@
+import { useState } from "react";
+
 interface AwaitThenableProps {
 	action: () => void;
 	children: React.ReactNode;
 }
 
 export function AwaitThenable({ action, children }: AwaitThenableProps) {
-	return <button onClick={action}>{children}</button>;
+	const [running, setRunning] = useState(false);
+
+	const onClick = async () => {
+		setRunning(true);
+		await action();
+		setRunning(false);
+	};
+
+	return (
+		<button disabled={running} onClick={onClick}>
+			{children}
+		</button>
+	);
 }
